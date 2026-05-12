46cd8d2b25fd9d0cb5648190f5112f056ef8bab6
ergolyam <grisha765@tuta.io>
2026-05-13 02:43:14 +0300

    [u] quoting

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,18 +8,18 @@ set -e
 : "${PRIVKEY_FILE:=/data/server.key}"
 : "${PUBKEY_FILE:=/data/server.pub}"
 
-if [ -z $DNS_ZONE ]; then
+if [ -z "${DNS_ZONE:-}" ]; then
     echo "ERROR: DNS_ZONE is not set." >&2
     echo "Please set your own DNS_ZONE, for example:" >&2
     echo "  DNS_ZONE=t.example.com" >&2
     exit 1
 fi
 
-mkdir -p $(dirname $PRIVKEY_FILE)
+mkdir -p "$(dirname "$PRIVKEY_FILE")"
 
-if [ ! -s $PRIVKEY_FILE ]; then
+if [ ! -s "$PRIVKEY_FILE" ]; then
     echo "dnstt private key not found, generating key pair..."
-    /app/dnstt-server -gen-key -privkey-file $PRIVKEY_FILE -pubkey-file $PUBKEY_FILE
+    /app/dnstt-server -gen-key -privkey-file "$PRIVKEY_FILE" -pubkey-file "$PUBKEY_FILE"
 fi
 
-exec /app/dnstt-server -udp :$PORT -privkey-file $PRIVKEY_FILE -mtu $MTU $DNS_ZONE $FORWARD
+exec /app/dnstt-server -udp ":$PORT" -privkey-file "$PRIVKEY_FILE" -mtu "$MTU" "$DNS_ZONE" "$FORWARD"
