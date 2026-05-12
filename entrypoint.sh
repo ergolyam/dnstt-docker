260bf6216717de6d4e81bc9b23b7ccd638f282df
ergolyam <grisha765@tuta.io>
2026-05-12 21:22:03 +0300

    [u] dns zone required

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,20 @@ set -e
 
 : "${PORT:=53}"
 : "${MTU:=1232}"
-: "${DNS_ZONE:=t.example.com}"
 : "${FORWARD:=127.0.0.1:1080}"
 : "${PRIVKEY_FILE:=/data/server.key}"
 : "${PUBKEY_FILE:=/data/server.pub}"
 
+if [ -z $DNS_ZONE ]; then
+    echo "ERROR: DNS_ZONE is not set." >&2
+    echo "Please set your own DNS_ZONE, for example:" >&2
+    echo "  DNS_ZONE=t.example.com" >&2
+    exit 1
+fi
+
 mkdir -p $(dirname $PRIVKEY_FILE)
 
-if [ ! -s "$PRIVKEY_FILE" ]; then
+if [ ! -s $PRIVKEY_FILE ]; then
     echo "dnstt private key not found, generating key pair..."
     /app/dnstt-server -gen-key -privkey-file $PRIVKEY_FILE -pubkey-file $PUBKEY_FILE
 fi
